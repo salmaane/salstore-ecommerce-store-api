de28806c0f295993b822b5369cf4d237c1efea01
Salmane <elmourtajisalmane@gmail.com>
2023-07-23 13:24:16 +0100

    added react SPA url in allowed origins for cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,9 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        env('FRONTEND_URL', 'http://localhost:5173')
+    ],
 
     'allowed_origins_patterns' => [],
 
